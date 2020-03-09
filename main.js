2fd630f72a63eeff54def962ade99a996e130ee8
Pawel Mrozikowski <mrozik.p@gmail.com>
2020-03-09 02:51:17 +0100

    feat: destructuring

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ fetch('https://api.github.com/users/PawelMrozikowski/repos')
 .then(resp => {
     const repos = resp;
     for(const repo of repos){
-    list.innerHTML += `<li><a href="${repo.url}"> ${repo.name}</a></li>`;
+        const{description, url, name}= repo;
+    list.innerHTML += `<li class="list__item"><a href class="list__link" ="${url}"> ${name}</a>
+    <p class="list__description">${description ? description : "brak opisu"} </p></li>`;
     }
 })
     
